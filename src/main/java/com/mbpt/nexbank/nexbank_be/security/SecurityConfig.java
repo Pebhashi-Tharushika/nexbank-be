dc8f965aa5ba208b1e260e55c18fa8bda42b461e
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-07-05 00:12:08 +0530

    enable cors in the filter chain

--- src/main/java/com/mbpt/nexbank/nexbank_be/security/SecurityConfig.java
+++ src/main/java/com/mbpt/nexbank/nexbank_be/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mbpt.nexbank.nexbank_be.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authz -> authz.anyRequest()
                         .authenticated())
                 .sessionManagement(session ->
